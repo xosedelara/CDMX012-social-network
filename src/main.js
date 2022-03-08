4804f61033d31cedc620962fba2b7e04e0b45b0c
xosedelara <97544706+xosedelara@users.noreply.github.com>
2022-03-08 16:29:56 -0600

    añadiendo form de registro

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
 // Este es el punto de entrada de tu aplicacion
+// import { myFunction } from './lib/index.js';
 
-import { myFunction } from './lib/index.js';
+const root = document.getElementById('root');
+const registerButton = document.getElementById('registerButton');
 
-myFunction();
+const showRegForm = () => {
+  const registrationBox = document.createElement('section');
+  const registrationForm = document.createElement('form');
+  const registrationName = document.createElement('input');
+  const registrationMail = document.createElement('input');
+  const registrationPW = document.createElement('input');
+  const registrationPW2 = document.createElement('input');
+  const registrationSubmit = document.createElement('button');
+  registrationBox.innerText = 'Crea un usuario y contraseña';
+  registrationName.setAttribute('placeholder', 'Nombre');
+  registrationMail.setAttribute('placeholder', 'Correo Electrónico');
+  registrationPW.setAttribute('placeholder', 'Contraseña');
+  registrationPW2.setAttribute('placeholder', 'Confirma contraseña');
+  registrationSubmit.setAttribute('value', 'Registrar');
+  registrationForm.append(registrationName, registrationMail, registrationPW, registrationPW2, registrationSubmit);
+  registrationBox.appendChild(registrationForm);
+  return registrationBox;
+};
+registerButton.addEventListener('click', () => {
+  root.innerHTML = '';
+  root.appendChild(showRegForm());
+});
+// myFunction();
