5bcf1c840bcf44ea6d8fe2171ec37d5924ad526e
maisahr <ma.isahr7@gmail.com>
2022-03-09 06:47:34 -0800

    Implementando el routing de inicio de sesion a registro y viceversa

---
+++ src/about.js
@@ -0,0 +1,18 @@
+let about = `
+<div>
+    <section class="reg-box">
+        <p>Crea un usuario y contraseña</p>
+        <form>
+            <input placeholder="Nombre"></input>
+            <input placeholder="Correo electrónico"></input>
+            <input placeholder="Contraseña"></input>
+            <input placeholder="Confirma contraseña"></input>
+        </form>
+        <button class="register-button">Regístrate</button>
+        <p>o regístrate con:</p>
+        <img src="img/facebook.png" class="facebook-logo">
+        <img src="img/google.png" class="gmail-logo">
+    </section>
+    <a href="#" onclick="onNavigate('/'); return false;"><button class="register-button">Regresa al inicio</button></a>
+</div>
+`
