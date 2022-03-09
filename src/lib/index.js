856665e514b682821d2c33a801717af13cbb6a29
Amaya De Esesarte Díaz González <ADEDG@MacBook-Air-de-Macintosh.local>
2022-03-09 00:19:41 -0600

    test firebase

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,82 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable indent */
+/* eslint-disable import/no-unresolved */
 // aqui exportaras las funciones que necesites
+
+
+// Este es el punto de entrada de tu aplicacion
+
+// import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js';
+// import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js';
+// // import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = initializeApp({
+//     apiKey: 'AIzaSyC26n4Fh-NfxC_ZNKZrFDH4NzrQrYwgirY',
+//     authDomain: 'petspace-3f65f.firebaseapp.com',
+//     projectId: 'petspace-3f65f',
+//     storageBucket: 'petspace-3f65f.appspot.com',
+//     messagingSenderId: '719999017536',
+//     appId: '1:719999017536:web:4e72654f1a5dba66b1b5a5',
+// });
+
+// // Initialize Firebase
+// export const auth = getAuth(firebaseConfig);
+// // createUserWithEmailAndPassword(auth, email, password)
+// //  .then((userCredential) => {
+// //     // Signed in
+// //     const user = userCredential.user;
+// //     // ...
+// //   })
+// //   .catch((error) => {
+// //     const errorCode = error.code;
+// //     const errorMessage = error.message;
+// //     // ..
+// //   });
+
+// // const db = getFirestore(firebaseConfig);
+// // db.setings({ timestampsInSnaphots: true});
+// onAuthStateChanged(auth, (user) => {
+//     if (user != null) {
+//         console.log('logged in!');
+//     } else {
+//         console.log('No user');
+//     }
+// });
+
+// // const auth = getAuth();
+// // createUserWithEmailAndPassword(auth, email, password)
+// //   .then((userCredential) => {
+// //     // Signed in
+// //     const user = userCredential.user;
+// //     // ...
+// //   })
+// //   .catch((error) => {
+// //     const errorCode = error.code;
+// //     const errorMessage = error.message;
+// //     // ..
+// //   });
+
+// const singupForm = document.getElementById('loginForm');
+// singupForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   // obtener la info del ususario
+//   const email = singupForm.user.value;
+//   const password = singupForm.password.value;
+//   console.log(email, password);
+//   // eslint-disable-next-line no-undef
+//   createUserWithEmailAndPassword(auth, email, password).then((cred) => {
+//     console.log(cred);
+//   });
+// });
+
+
 
 export const myFunction = () => {
   // aqui tu codigo
   console.log('Hola mundo!');
 };
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,61 @@
-// Este es el punto de entrada de tu aplicacion
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-empty */
+/* eslint-disable space-before-blocks */
+/* eslint-disable indent */
+/* eslint-disable no-multiple-empty-lines */
+// /* eslint-disable no-multiple-empty-lines */
 
-import { myFunction } from './lib/index.js';
+// import { auth } from './lib/index.js';
 
-myFunction();
+
+// // signup
+// const singupForm = document.getElementById('loginForm');
+// singupForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   // obtener la info del ususario
+//   const email = singupForm.user.value;
+//   const password = singupForm.password.value;
+//   console.log(email, password);
+//   // eslint-disable-next-line no-undef
+//   auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+//     console.log(cred);
+//   });
+// });
+
+
+
+
+
+// import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js';
+// import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js';
+
+// const firebaseConfig = {
+//     apiKey: 'AIzaSyC26n4Fh-NfxC_ZNKZrFDH4NzrQrYwgirY',
+//     authDomain: 'petspace-3f65f.firebaseapp.com',
+//     projectId: 'petspace-3f65f',
+//     storageBucket: 'petspace-3f65f.appspot.com',
+//     messagingSenderId: '719999017536',
+//     appId: '1:719999017536:web:4e72654f1a5dba66b1b5a5',
+// };
+
+// firebase.initializeApp(firebaseConfig);
+
+// const singupForm = document.getElementById('loginForm');
+// singupForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const email = singupForm.user.value;
+//   const password = singupForm.password.value;
+//   console.log(email, password);
+//   // eslint-disable-next-line no-undef
+//   firebase.auth().createUserWithEmailAndPassword(email, password).catch(e => {
+//     console.log(e);
+//   });
+// });
+// firebase.auth().onAuthStateChanged((user) => {
+//     if (user){
+//         console.log(user);
+//     } else {
+        
+//     }
+// });
