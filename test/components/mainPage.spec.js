1cea5bad7293d711697f08d0fd408c28bb7082b8
maisahr <ma.isahr7@gmail.com>
2022-04-08 09:22:36 -0700

    Primer test de mainPage (no funciona)

--- test/components/mainPage.spec.js
+++ test/components/mainPage.spec.js
@@ -1,8 +1,15 @@
 import { mainPage } from '../../src/components/mainPage.js';
+import * as app from '../../src/app.js';
 import firebase from '../../src/lib/__mocks__/firebase-Auth.js';
 
 describe('mainPage', () => {
   it('is a function', () => {
-    expect(typeOf mainPage).toBe('function');
+    expect(typeof mainPage).toBe('function');
+  });
+  it('menu button takes you to home', () => {
+    const menuBtn = document.getElementById('menuIcon');
+    const mySpy = jest.spyOn(app, 'onNavigate');
+    menuBtn.click();
+    expect(mySpy).toHaveBeenCalled();
   });
 });
