5d6f4d3fd00ff87409a091cbbbf72691361bc372
maisahr <ma.isahr7@gmail.com>
2022-04-08 11:22:36 -0700

    Tests de mainPage, no funcionan

--- test/components/mainPage.spec.js
+++ test/components/mainPage.spec.js
@@ -1,15 +1,22 @@
+/**
+ * @jest-environment jsdom
+ */
 import { mainPage } from '../../src/components/mainPage.js';
-import * as app from '../../src/app.js';
+/* import * as app from '../../src/app.js'; */
 import firebase from '../../src/lib/__mocks__/firebase-Auth.js';
 
-describe('mainPage', () => {
-  it('is a function', () => {
-    expect(typeof mainPage).toBe('function');
-  });
-  it('menu button takes you to home', () => {
-    const menuBtn = document.getElementById('menuIcon');
-    const mySpy = jest.spyOn(app, 'onNavigate');
-    menuBtn.click();
-    expect(mySpy).toHaveBeenCalled();
+const component = mainPage();
+document.body.innerHTML = '<div id="root"></div>';
+const rootDiv = document.getElementById('root');
+function reloadMainPage() {
+  window.location.pathname = '/mainPage';
+  rootDiv.innerHTML = '';
+  rootDiv.appendChild(component);
+}
+
+describe('Main page', () => {
+  it('Snapshot main page', () => {
+    reloadMainPage();
+    expect(rootDiv.innerHTML).toMatchSnapshot();
   });
 });
